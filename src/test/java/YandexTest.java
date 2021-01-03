4662a5f7fc165dc5d91f319f9bcbb1b7aa569c8e
irinastyazhkina <i.styazhkina@propellerads.net>
2021-01-03 18:44:06 +0700

    add test

---
+++ src/test/java/YandexTest.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
+class YandexTest {
+
+    @BeforeEach
+    void openPage() {
+        open("https://yandex.ru");
+    }
+
+    @Test
+    void searchTest() {
+        $(".input__control").setValue("Новосибирск").pressEnter();
+        $$(".serp-item")
+                .get(0).shouldHave(Condition.text("Новосибирск"));
+
+    }
+
+
+    @AfterEach
+    void closeBrowser() {
+        Selenide.closeWebDriver();
+    }
+}
